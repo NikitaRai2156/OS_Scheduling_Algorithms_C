297f91fea87f6b2a8ca976f914b42a2e2257f06a
Nikita Rai <nikrc15@gmail.com>
2017-05-26 12:39:11 +0530

    Create FirstComeFirstServeCPUScheduling.c

---
+++ FirstComeFirstServeCPUScheduling.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int n, bt[20], wt[20], tat[20], avgtat=0, avgwt=0, i, j;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter burst time for:\n");
+    for(i=0;i<n;i++){
+        printf("Process %d: ", i+1);
+        scanf("%d", &bt[i]);
+        printf("\n");
+    }
+
+    wt[0] = 0; //first process wt = 0
+
+    for(i=1;i<n;i++){
+        wt[i] = 0;
+        for(j=0;j<i;j++){
+            wt[i]+=bt[j];
+        }
+    }
+
+    printf("\nPROCESS\t\tBURST TIME\t\tWAIT TIME\t\tTURNAROUND TIME");
+
+    for(i=0;i<n;i++){
+        tat[i] = bt[i] + wt[i];
+        avgtat += tat[i];
+        avgwt += wt[i];
+        printf("\nP[%d]\t\t%d\t\t\t%d\t\t\t%d", i+1, bt[i], wt[i], tat[i]);
+    }
+
+    avgwt = avgwt/n;
+    avgtat = avgtat/n;
+
+    printf("\nAverage Waiting Time: %f ms", avgwt);
+    printf("\nAverage Turnaround Time: %f ms", avgtat);
+}
